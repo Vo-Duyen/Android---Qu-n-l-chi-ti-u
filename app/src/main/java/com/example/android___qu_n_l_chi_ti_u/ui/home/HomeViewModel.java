12244339d493b81033b1fd3cdd5ac7eebacb8299
trung-epicsoft4 <quoctrung@epicsoft4.com>
2024-11-22 03:55:31 +0700

    begin

---
+++ app/src/main/java/com/example/android___qu_n_l_chi_ti_u/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.android___qu_n_l_chi_ti_u.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/example/android___qu_n_l_chi_ti_u/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.android___qu_n_l_chi_ti_u.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
